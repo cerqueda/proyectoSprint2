09ff1355638f27d12e1adfabb3674855763aee90
Cecilia Cerqueda <pzcerqueda@gmail.com>
2017-03-29 12:05:28 -0600

    version3 sprint3

--- trello.js
+++ trello.js
@@ -1,11 +1,3 @@
-
-function convertToF(celsius) {
-    var cTempVal = parseFloat(document.getElementById('celsius').value);
-    var farenheit = document.getElementById('farenheit');
-    var fTempVal = (cTempVal * (9/5)) + 32;
-    farenheit.value=fTempVal;
-}
-
 function agregarPendiente(valorNuevoItem) {
     var nuevoItem = (document.getElementById('valorNuevoItem').value);
     var listaPendientes = document.getElementById('lista');
