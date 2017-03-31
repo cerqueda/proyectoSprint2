22182ad00f6529a48de586d53d7b097dcde80cf4
Cecilia Cerqueda <pzcerqueda@gmail.com>
2017-03-31 09:46:43 -0600

    version final sprint2

--- trello.js
+++ trello.js
@@ -1,7 +1,14 @@
-function listaPendientes(valorNuevoPendiente) {
+/*function agregarPendientes() {
     var input = document.createElement("input");
     var listaPe = document.createTextNode("");
     nuevoPendiente.appendChild(listaPe);
     document.body.appendChild(nuevoPendiente)
 
+}*/
+
+function creaLista(){
+  var pendiente = document.getElementById('pendiente').value;
+  var lista = document.getElementById('lista');
+  lista.innerHTML = lista;
+  console.log(lista);
 }
